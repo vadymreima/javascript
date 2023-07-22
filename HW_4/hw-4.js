7708dcb921c35f140156c3aa48de0fc686dc7a42
vadymreima <vadym.reima@gmail.com>
2023-07-23 01:30:03 +0300

    =

--- HW_4/hw-4.js
+++ HW_4/hw-4.js
@@ -1,17 +1,26 @@
 // ЗАДАЧА №1
 
 let animals = ['cat', 'cow', 'fish', 'chicken', 'dog', 'pig'];
+let animals1 = ['cat1', 'cow1', 'fish1', 'chicken1', 'dog1', 'pig1'];
 
-function getAnimalFn(argument, item) {
-    if (item == "first") {
-        console.log(argument[0]);
+function getAnimalFn(animalsArr, item) {
+    if (item == 'first') {
+        console.log(animalsArr[0]);
     }
-    if (item == "last") {
-        console.log(animals[argument.length - 1])
+    else if (item == 'last') {
+        console.log(animalsArr[animalsArr.length - 1])
     }
+        if ( item != 'first') {
+            console.log('error')
+        }
+        else if ( item != 'last') {
+            console.log('error')
+        }
 }
-getAnimalFn(animals, 'first')
 getAnimalFn(animals, 'last')
+
+
+
 
 
 
@@ -19,9 +28,10 @@ getAnimalFn(animals, 'last')
 
 // ЗАДАЧА №2
 
-let smileArr = [':)', '=)',':)', '=)',':)', '=)'];
+let smile = [':)', '=)',':)', '=)',':)', '=)'];
+let smileMore = [':)', '=)',':)', '=)',':)', '=)', ':)', ];
 
-function changeSmile(newSmile){
+function changeSmile(newSmile, smileArr){
     for(i = 0; i < smileArr.length; i++){
         if(smileArr[i] == ':)') {
             smileArr[i] = newSmile;
@@ -29,6 +39,8 @@ function changeSmile(newSmile){
     }
     console.log(smileArr);
 }
-changeSmile('*')
+changeSmile('=)))', smile)
+changeSmile('=))))))))))))', smileMore)
+
 
 
