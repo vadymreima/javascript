63fe6f07bc7e843693670c6cd8f60a88771bab17
vadymreima <vadym.reima@gmail.com>
2023-07-26 00:56:22 +0300

    +

---
+++ HW_6/hw-6.js
@@ -0,0 +1,53 @@
+// // - Створити функцію яка повертає сторіччя, функція отримує рік
+
+// function getCentury (year){
+//     return Math.floor(year / 100) + 1;
+// }
+// console.log(getCentury(1175));
+
+// ///////// OR /////////
+
+// let year2 = 1993;
+// let century = getCentury(year2) + 1;
+// console.log(century);
+
+
+
+
+
+
+// // - Реалізуйте гру камінь- нижниці - бумага за допомогою методів розглянутих на уроці
+
+// function game() {
+//     const choices = ['папір', 'камінь', 'ножиці'];
+//     const playerChoice = prompt('Що вибираєш: папір, ножиці чи камінь').toLowerCase();
+//     const computerChoice = choices[Math.floor(Math.random() * 3)];
+  
+//     alert(`Ти обрав: ${playerChoice}, компютер обрав ${computerChoice}`);
+  
+//     if (playerChoice === computerChoice) {
+//       alert('Нічія!');
+//     } else if (
+//         (playerChoice === 'ножиці' && computerChoice === 'папір') || (playerChoice === 'камінь' && computerChoice === 'ножиці') || (playerChoice === 'папір' && computerChoice === 'камінь')
+//     ) {
+//       alert('Гравець виграв!');
+//     } else {
+//       alert('Комп`ютер виграв!');
+//     }
+//   }
+//   game();
+  
+
+
+
+
+// - Створити функцію яка отримує массив і тип.В функції  з массиву вибираються  всі типи що були передані  і повертаються новим массивом
+
+const sort = [true, 'false', 111, -0, 'true', false, {a:false}, [true,'false']];
+
+function filterArr(arr, type) {
+    return arr.filter(item => typeof item === type);
+  }
+
+let newArray = filterArr(sort, 'string');
+console.log(newArray)
