2e4d1bf5527e40a5df3c4e31c349b70248eef089
vadymreima <vadym.reima@gmail.com>
2023-07-26 01:00:01 +0300

    +

--- HW_6/hw-6.js
+++ HW_6/hw-6.js
@@ -1,41 +1,41 @@
-// // - Створити функцію яка повертає сторіччя, функція отримує рік
+// - Створити функцію яка повертає сторіччя, функція отримує рік
 
-// function getCentury (year){
-//     return Math.floor(year / 100) + 1;
-// }
-// console.log(getCentury(1175));
+function getCentury (year){
+    return Math.floor(year / 100) + 1;
+}
+console.log(getCentury(1175));
 
-// ///////// OR /////////
+///////// OR /////////
 
-// let year2 = 1993;
-// let century = getCentury(year2) + 1;
-// console.log(century);
-
-
+let year2 = 1993;
+let century = getCentury(year2) + 1;
+console.log(century);
 
 
 
 
-// // - Реалізуйте гру камінь- нижниці - бумага за допомогою методів розглянутих на уроці
 
-// function game() {
-//     const choices = ['папір', 'камінь', 'ножиці'];
-//     const playerChoice = prompt('Що вибираєш: папір, ножиці чи камінь').toLowerCase();
-//     const computerChoice = choices[Math.floor(Math.random() * 3)];
+
+// - Реалізуйте гру камінь- нижниці - бумага за допомогою методів розглянутих на уроці
+
+function game() {
+    const choices = ['папір', 'камінь', 'ножиці'];
+    const playerChoice = prompt('Що вибираєш: папір, ножиці чи камінь').toLowerCase();
+    const computerChoice = choices[Math.floor(Math.random() * 3)];
   
-//     alert(`Ти обрав: ${playerChoice}, компютер обрав ${computerChoice}`);
+    alert(`Ти обрав: ${playerChoice}, компютер обрав ${computerChoice}`);
   
-//     if (playerChoice === computerChoice) {
-//       alert('Нічія!');
-//     } else if (
-//         (playerChoice === 'ножиці' && computerChoice === 'папір') || (playerChoice === 'камінь' && computerChoice === 'ножиці') || (playerChoice === 'папір' && computerChoice === 'камінь')
-//     ) {
-//       alert('Гравець виграв!');
-//     } else {
-//       alert('Комп`ютер виграв!');
-//     }
-//   }
-//   game();
+    if (playerChoice === computerChoice) {
+      alert('Нічія!');
+    } else if (
+        (playerChoice === 'ножиці' && computerChoice === 'папір') || (playerChoice === 'камінь' && computerChoice === 'ножиці') || (playerChoice === 'папір' && computerChoice === 'камінь')
+    ) {
+      alert('Гравець виграв!');
+    } else {
+      alert('Комп`ютер виграв!');
+    }
+  }
+  game();
   
 
 
@@ -46,7 +46,7 @@
 const sort = [true, 'false', 111, -0, 'true', false, {a:false}, [true,'false']];
 
 function filterArr(arr, type) {
-    return arr.filter(item => typeof item === type);
+    return arr.filter(i => typeof i === type);
   }
 
 let newArray = filterArr(sort, 'string');
